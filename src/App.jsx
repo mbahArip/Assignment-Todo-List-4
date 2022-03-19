dcb2a7fe5bdc79ab098d69665caf2a02a132f886
mbaharip <main@mbaharip.me>
2022-03-19 20:52:50 +0700

    Update menggunakan class.

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,90 @@
-import { useState } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import { default as ListItem } from './components/ListItem';
 
-const App = () => {
-	const [todoInput, setTodoInput] = useState(null);
-	const [todoList, setTodoList] = useState([]);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			todoInput: '',
+			todoList: [],
+		};
+	}
 
-	const inputHandler = (e) => {
-		e.preventDefault();
+	inputHandler = (event) => {
+		event.preventDefault();
 
-		if (!todoInput || todoInput === '') {
-			return alert("Todo can't be empty!");
+		if (this.state.todoInput === '') {
+			return alert('Please enter a todo.');
 		}
-		setTodoList([...todoList, { id: todoList.length + 1, title: todoInput, complete: false }]);
-		e.target.reset();
-		setTodoInput('');
+		this.setState({ todoList: [{ id: this.state.todoList.length + 1, title: this.state.todoInput, complete: false }, ...this.state.todoList] });
+		event.target.reset();
+		this.setState({ todoInput: '' });
 	};
 
-	const checkHandler = (e) => {
-		const newTodoList = [...todoList];
-		const itemID = parseInt(e.target.id) - 1;
-		newTodoList[itemID].complete = !newTodoList[itemID].complete;
-		setTodoList(newTodoList);
+	checkHandler = (event) => {
+		// Get list
+		const itemID = parseInt(event.target.id);
+		let updatedItem = this.state.todoList.find((item) => item.id === itemID);
+		updatedItem = [{ ...updatedItem, complete: !updatedItem.complete }];
+		let newList = this.state.todoList.map((item) => updatedItem.find((obj) => obj.id === item.id) || item);
+		this.setState({ todoList: newList });
+
+		// let findItem = this.state.todoList.find((item) => item.id === itemID);
+		// findItem = { ...findItem, complete: !findItem.complete };
+		// this.setState({ todoList: [...this.state.todoList, findItem] });
 	};
 
-	const deleteHandler = (e) => {
-		const itemID = parseInt(e.target.id);
-		const filterList = todoList.filter((item) => item.id !== itemID);
-		setTodoList(filterList);
+	deleteHandler = (event) => {
+		const itemID = parseInt(event.target.id) - 1;
+		const filterList = this.state.todoList.filter((item) => item.id !== itemID + 1);
+		this.setState({ todoList: filterList });
 	};
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.title}>
-				<h1 className={styles.titleBig}>DoIt!</h1>
-				<span className={styles.titleSmall}>Todo tracker App</span>
-			</div>
-			<form
-				className={styles.inputForm}
-				onSubmit={(e) => {
-					inputHandler(e);
-				}}
-			>
-				<input
-					placeholder='Add todo...'
-					className={styles.inputTodo}
-					onChange={(e) => {
-						setTodoInput(e.target.value);
+	render() {
+		return (
+			<div className={styles.container}>
+				<div className={styles.title}>
+					<h1 className={styles.titleBig}>DoIt!</h1>
+					<span className={styles.titleSmall}>Todo tracker App</span>
+				</div>
+				<form
+					className={styles.inputForm}
+					onSubmit={(e) => {
+						this.inputHandler(e);
 					}}
-				/>
-				<button type='submit' className={styles.inputButton}>
-					Submit
-				</button>
-			</form>
-			<div className={styles.listTodo}>
-				{todoList.length === 0 && <span className={styles.noList}>There are no list item.</span>}
-				{todoList.map((item) => (
-					<ListItem
-						key={item.id}
-						id={item.id}
-						title={item.title}
-						complete={item.complete}
-						checkBoxCB={(e) => {
-							checkHandler(e);
-						}}
-						buttonCB={(e) => {
-							deleteHandler(e);
+				>
+					<input
+						placeholder='Add todo...'
+						className={styles.inputTodo}
+						onChange={(e) => {
+							this.setState({ todoInput: e.target.value });
 						}}
 					/>
-				))}
+					<button type='submit' className={styles.inputButton}>
+						Submit
+					</button>
+				</form>
+				<div className={styles.listTodo}>
+					{this.state.todoList.length === 0 && <span className={styles.noList}>There are no item in the list.</span>}
+					{this.state.todoList.map((item) => (
+						<ListItem
+							key={item.id}
+							id={item.id}
+							title={item.title}
+							complete={item.complete}
+							checkboxCallback={(e) => {
+								this.checkHandler(e);
+							}}
+							deleteCallback={(e) => {
+								this.deleteHandler(e);
+							}}
+						/>
+					))}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default App;
